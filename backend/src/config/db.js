058e07c7a8270aeb50920b6afc98c4d84c2ca640
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2026-01-23 10:48:20 +0530

    fixed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,19 +2,17 @@ const mongoose = require("mongoose");
 const logger = require("./logger");
 const config = require("./env");
 
+// Set global mongoose options
+mongoose.set('bufferTimeoutMS', 30000);
+mongoose.set('bufferCommands', false);
+
 const connectDB = async () => {
   try {
-    // Check if already connected
-    if (mongoose.connection.readyState === 1) {
-      return mongoose.connection;
-    }
-
     const conn = await mongoose.connect(config.mongodb.uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
       socketTimeoutMS: 45000, // Socket timeout
-      bufferTimeoutMS: 30000, // Buffer timeout to 30 seconds
       maxPoolSize: 10, // Maintain up to 10 socket connections
     });
 
